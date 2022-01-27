4d3feb0fa4a2d0b30c94fa382c5e73f52fe61e10
BernardKwesi <bernarddormon31@gmail.com>
2022-01-27 14:52:06 +0000

    First Commit without Styling

---
+++ routes/index.js
@@ -0,0 +1,37 @@
+var express = require('express');
+var router = express.Router();
+const moment = require('moment');
+
+const messages =[
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date()
+  },{
+    text:"Hello World!",
+    user:"Charles",
+    added : new Date()
+  }
+];
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Mini Message Board',
+messages:messages });
+});
+
+
+
+router.post('/new', (req, res, next) => {
+     
+      let newMessage = {
+        text :  req.body.message,
+        user : req.body.user,
+        added : moment()
+        .startOf('hour' - 1)
+        .fromNow(),
+      }
+
+      messages.unshift(newMessage);
+      res.redirect('/');
+});
+module.exports = router;
